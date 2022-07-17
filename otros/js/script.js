e7133d0cd5a952ba58830dc07e0890585058a6a7
BlueberryWeb <blueberryweb7@gmail.com>
2022-07-16 21:51:31 -0500

    .

--- otros/js/script.js
+++ otros/js/script.js
@@ -14,3 +14,43 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 /*TERMINA COLLAPSE DE CATEGORIAS */
+//CAMBIAR IMAGEN EN EL HOVER DE LOS PRODUCTOS
+/*DETALLE */
+window.addEventListener('load', detalle, false);
+function detalle() {      
+    var imagen = document.getElementById('detalle');
+    imagen.addEventListener('mouseover', peligro_detalle, false);
+    imagen.addEventListener('mouseout', restaurar_detalle, false);
+}
+function restaurar_detalle(){
+    var imagen = document.getElementById('detalle').src = "img/icono-ver-hover.png";
+}
+function peligro_detalle() {
+    var imagen = document.getElementById('detalle').src = "img/icono-ver.gif";
+}
+/*WHISLIST*/
+window.addEventListener('load', fav, false);
+function fav() {      
+    var imagen = document.getElementById('fav');
+    imagen.addEventListener('mouseover', peligro_fav, false);
+    imagen.addEventListener('mouseout', restaurar_fav, false);
+}
+function restaurar_fav(){
+    var imagen = document.getElementById('fav').src = "img/icono-fav.png";
+}
+function peligro_fav() {
+    var imagen = document.getElementById('fav').src = "img/icono-Wishlist.gif";
+}
+/*CARRITO */
+window.addEventListener('load', carrito, false);
+function carrito() {      
+    var imagen = document.getElementById('carrito');
+    imagen.addEventListener('mouseover', peligro_carrito, false);
+    imagen.addEventListener('mouseout', restaurar_carrito, false);
+}
+function restaurar_carrito(){
+    var imagen = document.getElementById('carrito').src = "img/icono-carrito-hover.png";
+}
+function peligro_carrito() {
+    var imagen = document.getElementById('carrito').src = "img/icono-Carrito.gif";
+}
